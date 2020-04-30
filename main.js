4ebecc00f8693a81bab91d81cdab5e451d89c4e0
Pete Stewart <petestew@gmail.com>
2020-04-29 21:33:20 -0500

    initial javascript working. console logging student name

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('sorting the hat');
+
+
+const clickEvents = () => {
+    document.querySelector('#start').addEventListener('click', showForm);
+    document.querySelector('#studentForm').addEventListener('submit', getName);
+}
+
+const showForm = () => {
+    formArea.style.display = 'block';
+}
+
+
+const getName = (event) => {
+    const studentName = event.target[0].value;
+    buildCard (studentName);
+}
+
+const buildCard = (name) => {
+    console.log(name);
+}
+
+
+init = () => {
+    formArea.style.display = 'none';
+    clickEvents();
+}
+
+init();
+
+
+    
+
+// const form = document.forms[0];
+
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+// })
